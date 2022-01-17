56f0a31effd0545c381ccad84e1c33ce6f6b9788
martian17 <yakumodanshi@gmail.com>
2022-01-17 15:55:06 +0900

    created some basic css and methods for node

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 ELEM.overrideAddMethod();
 
 class Graph_Joint extends ELEM{
-    
+    constructor(){
+        super("div","class:joint");
+        this.on("mouseenter",()=>{
+
+        });
+    }
+    connect(spline){
+        ;
+    }
 }
 
 class Graph_Left extends ELEM{
@@ -17,20 +25,61 @@ class Graph_Left extends ELEM{
 class Graph_Node extends ELEM{
     constructor(){
         super("div","class:node;");
-        this.left = this.add("div");
-        this.right = this.add("div");
+        this.in = this.add("div");
+        this.out = this.add("div");
         this.lefts = new ELEM("div");
     }
     addLeftConnector(){
         this.left.add("div","class:");
     }
-    
+}
+
+
+class Graph_Node extends ELEM{
+    constructor(){
+        super("div","class:node;");
+    }
+    outs(){
+        let outs = this.out.children.toArray();
+        return outs.map(out=>{
+            out.spline.otherEnd(out);
+        });
+    }
+    ins(){
+        return this.in.children.toArray();
+    }
 }
 
 
 const main = function(){
+
+
+    let field = new NodeField();
+    field.add();
+
+
     let gn = body.add(new Graph_Node());
     gn.left.add();
     gn.right.add();
+
+
 }
+
+
+
+//understanding promise better
+
+let Promise1 = function(cb){
+    this.then = function(cb){
+        this.next = cb;
+        return this;
+    }
+    this.resolve = function(){
+        if(this.next)this.next();
+    }
+    cb(this.resolve);
+}
+
+
+
 
